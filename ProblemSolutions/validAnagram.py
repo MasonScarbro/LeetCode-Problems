09b2e6bcca056053108d2680951eab8af5c56f12
MasonScarbro <120221711+MasonScarbro@users.noreply.github.com>
2023-06-18 02:02:33 -0400

    Update validAnagram.py

--- ProblemSolutions/validAnagram.py
+++ ProblemSolutions/validAnagram.py
@@ -3,4 +3,4 @@ class Solution:
         if (sorted(s) == sorted(t)):
             return True
 
-// I actually did this in java and truthfully its not that mush longer its about 8 lines but stille basically 2 lines to compare two arrays is insane
+# I actually did this in java and truthfully its not that mush longer its about 8 lines but stille basically 2 lines to compare two arrays is insane
